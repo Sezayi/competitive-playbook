019103420716304da80116900ef5613c1c177903
Tom Klaver <tomklav@gmail.com>
2021-09-14 17:13:47 +0200

    add redirect

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body.full_name);
-  res.statusCode = 200;
-  res.json({ name: `John Doe` });
+
+  // iets als `url = strip.createCheckout(naam, etc.)`;
+  res.redirect("https://url-from-stripe");
+  // res.statusCode = 200;
+  // res.json({ name: `John Doe` });
 };
